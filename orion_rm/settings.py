3a896f5946fbd136f704094e1857d78cd14a3ea9
Lwmay <45488236+Lwmay@users.noreply.github.com>
2020-07-29 13:50:53 +0400

    UI: add bootstrap theme.
    Create Layout and index pages.

--- orion_rm/settings.py
+++ orion_rm/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'orion_rm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'orion_rm',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
@@ -119,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'),]
